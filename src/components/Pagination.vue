be493e18ef72e0d5cd8bc2c0b168fe4fd96c5c44
Henrik Nilsson Harnert <henrik.nilsson.harnert@gmail.com>
2019-05-20 22:33:34 +0200

    Create pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pagination">
+    <div @click="$store.commit('prevPagination')">
+      <b-icon
+        icon="arrow-left"
+        size="is-small"
+        v-if="$store.state.pagination > 0"
+      >
+      </b-icon>
+    </div>
+    <div>{{ store.state.pagination + 1 }}</div>
+    <div @click="$store.commit('nextPagination')">
+      <b-icon icon="arrow-right" size="is-small"> </b-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pagination",
+  props: ["store"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/buefy.scss";
+
+.pagination {
+  justify-content: center;
+  padding: $gap 0px;
+
+  div {
+    width: 50px;
+    user-select: none;
+  }
+}
+</style>
