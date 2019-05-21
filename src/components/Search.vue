1c3c43cff876dafa749cd713c8c9ced6416efc61
Henrik Nilsson Harnert <henrik.nilsson.harnert@gmail.com>
2019-05-21 13:08:00 +0200

    Implement start and end opening dates to filter

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -17,35 +17,39 @@
           Critics pick
         </b-switch>
       </b-field>
-      <b-field label="Select a date">
+      <b-field>
         <b-datepicker
-          placeholder="Type or select a date..."
+          v-model="openingDateStart"
+          placeholder="Opening date start..."
           :first-day-of-week="1"
+          :date-formatter="formatDate"
           icon="calendar-today"
           editable
         >
-          <button class="button" @click="date = new Date()">
+          <button class="button" @click="openingDateStart = new Date()">
             <b-icon icon="calendar-today"></b-icon>
             <span>Today</span>
           </button>
 
-          <button class="button" @click="date = null">
+          <button class="button" @click="openingDateStart = null">
             <b-icon icon="close"></b-icon>
             <span>Clear</span>
           </button>
         </b-datepicker>
         <b-datepicker
-          placeholder="Type or select a date..."
+          v-model="openingDateEnd"
+          placeholder="Opening date end..."
           :first-day-of-week="1"
+          :date-formatter="formatDate"
           icon="calendar-today"
           editable
         >
-          <button class="button" @click="date = new Date()">
+          <button class="button" @click="openingDateEnd = new Date()">
             <b-icon icon="calendar-today"></b-icon>
             <span>Today</span>
           </button>
 
-          <button class="button" @click="date = null">
+          <button class="button" @click="openingDateEnd = null">
             <b-icon icon="close"></b-icon>
             <span>Clear</span>
           </button>
@@ -75,6 +79,20 @@ export default {
   methods: {
     toggleAdvanced: function() {
       this.showAdvanced = !this.showAdvanced;
+    },
+    formatDate: function(d) {
+      console.log(d);
+      const year = d
+        .getFullYear()
+        .toString()
+        .padStart(4, "0");
+      const month = (d.getMonth() + 1).toString().padStart(2, "0");
+      const date = d
+        .getDate()
+        .toString()
+        .padStart(2, "0");
+      console.log(year, month, date);
+      return `${year}-${month}-${date}`;
     }
   },
   computed: {
@@ -97,6 +115,31 @@ export default {
       },
       set(value) {
         this.$store.commit("updateCriticsPick", value);
+      }
+    },
+    openingDateStart: {
+      get() {
+        let openingDate = this.$store.state["opening-date"];
+
+        return openingDate ? new Date(openingDate.split(";")[0]) : null;
+      },
+      set(value) {
+        this.$store.commit("updateOpeningDateStart", value);
+      }
+    },
+    openingDateEnd: {
+      get() {
+        let openingDate = this.$store.state["opening-date"];
+
+        if (openingDate) {
+          const split = openingDate.split(";");
+          if (split.length > 1) return new Date(split[1]);
+        }
+
+        return null;
+      },
+      set(value) {
+        this.$store.commit("updateOpeningDateEnd", value);
       }
     }
   }
--- src/stores/search.js
+++ src/stores/search.js
@@ -1,10 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import reviews from "@/stores/reviews";
+import { NotificationProgrammatic as Notification } from "buefy/dist/components/notification";
 
 Vue.use(Vuex);
 
 let typingTimeout = null;
+
+let formatDate = value => {
+  const d = new Date(value);
+
+  const year = d
+    .getFullYear()
+    .toString()
+    .padStart(4, "0");
+  const month = (d.getMonth() + 1).toString().padStart(2, "0");
+  const date = d
+    .getDate()
+    .toString()
+    .padStart(2, "0");
+
+  return `${year}-${month}-${date}`;
+};
 
 const store = new Vuex.Store({
   state: {
@@ -39,6 +56,65 @@ const store = new Vuex.Store({
     },
     updateCriticsPick(state, value) {
       state["critics-pick"] = value;
+      reviews.dispatch("reset", {
+        searchReset: false
+      });
+    },
+    updateOpeningDateStart(state, value) {
+      const date = formatDate(value);
+      const oldDates = state["opening-date"]
+        ? state["opening-date"].split(";")
+        : [null, null];
+
+      if (oldDates[1]) {
+        state["opening-date"] = `${date};${oldDates[1]}`;
+      } else {
+        state["opening-date"] = date;
+      }
+
+      reviews.dispatch("reset", {
+        searchReset: false
+      });
+    },
+    updateOpeningDateEnd(state, value) {
+      const date = formatDate(value);
+      const oldDates = state["opening-date"]
+        ? state["opening-date"].split(";")
+        : [null, null];
+
+      // Handle if not start date is set
+      if (!oldDates[0]) {
+        Notification.open({
+          duration: 5000,
+          message:
+            "It is not possible to set only a end date. Please chose a opening start date first.",
+          position: "is-bottom-right",
+          type: "is-warning",
+          hasIcon: true
+        });
+
+        // Do not set value if this error occurs
+        state["opening-date"] = null;
+        return;
+      }
+
+      // Handle if end date is before start date
+      console.log(new Date(oldDates[0]) > new Date(oldDates[1]));
+      if (new Date(oldDates[0]) > new Date(oldDates[1])) {
+        Notification.open({
+          duration: 5000,
+          message: "End date has to be after start date",
+          position: "is-bottom-right",
+          type: "is-warning",
+          hasIcon: true
+        });
+
+        // Do not set value if this error occurs
+        state["opening-date"] = oldDates[0];
+        return;
+      }
+
+      state["opening-date"] = `${oldDates[0]};${date}`;
       reviews.dispatch("reset", {
         searchReset: false
       });
